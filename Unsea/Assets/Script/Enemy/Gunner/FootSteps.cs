64b8cae646a3d8543f28c695ee1949acb865639d
o0Takumin0o <dark.damon.14@gmail.com>
2020-09-19 16:19:39 +0700

    map 3-4-5

---
+++ Unsea/Assets/Script/Enemy/Gunner/FootSteps.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] StepSound;
+    
+    private AudioSource audioSource;
+    
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    private void Step()
+    {
+        AudioClip clip = StepSound[UnityEngine.Random.Range(0, StepSound.Length)];
+        audioSource.PlayOneShot(clip);
+    }
+
+    
+}
